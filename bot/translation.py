b51f15d368b36743a9b57b721059b5c58d7428a2
Haseebrb <81767951+ah3653070@users.noreply.github.com>
2021-08-01 09:02:33 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,13 +5,13 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am  A Advance Auto Filter Bot.... And most useful for anyone
+<i>𝙸𝚊𝚖 𝚊 𝚊𝚍𝚟𝚊𝚗𝚌𝚎 𝚘𝚞𝚝𝚘 𝚏𝚒𝚕𝚝𝚎𝚛 𝚋𝚘𝚝
 
-Please Me To Your Group And Channel And Connect Them And See My power
+🔶 𝙰𝚗𝚢 𝚘𝚗𝚎 𝚌𝚊𝚗 𝚞𝚜𝚎 𝚖𝚎
+🔶 𝚌𝚊𝚗 𝚌𝚘𝚗𝚗𝚎𝚝 𝚠𝚒𝚝𝚑   𝚞𝚗𝚕𝚒𝚖𝚒𝚝𝚎𝚍 𝚐𝚛𝚘𝚞𝚙𝚜 𝚊𝚗𝚍 𝚌𝚑𝚊𝚗𝚗𝚎𝚕𝚜
 
-Click Help Button Below For More Details ....
-@Royalbotz
-</i>"""    
+🔖 𝙰𝚍𝚍 𝚖𝚎 𝚝𝚘 𝚢𝚘𝚞𝚛𝚎 𝚐𝚛𝚘𝚞𝚙𝚜 𝚊𝚗𝚍 𝚌𝚑𝚊𝚗𝚗𝚎𝚕𝚜 𝚠𝚎 𝚌𝚊𝚗 𝚖𝚊𝚗𝚊𝚐𝚎 𝚙𝚘𝚠𝚎𝚛 𝚏𝚞𝚕𝚕𝚢👀
+ ©@Royalbotz</i>"""    
     
     HELP_TEXT = """
 <b><i><u>How To Use Me!?</u></i></b>
@@ -26,13 +26,14 @@ Click Help Button Below For More Details ....
 © @Royalbotz
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>🔻 Name</b> : <code> Auto Filter Bot</code>
     
-🔻Creator  : <b><i>@haseebRb ,@The_God_of_devil</i>
+🔻Creator  : <b><i>@haseebRb ,
+              @The_God_of_devil</i>
 
 🔻Language : <code>Python3</code>
 
-🔻 Library : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+🔻 Library : <i><a href="docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
  Updat channel: <i><a href="https://t.me/Royalbotz">© Royalbotz</a></i>
 """

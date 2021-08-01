7c8ee673f29116cf174c840fc08f1b32d9076087
(◍•ᴗ•◍) <79841865+loverboyXD@users.noreply.github.com>
2021-08-01 14:11:53 +0530

    Add files via upload

---
+++ button.py
@@ -0,0 +1,8 @@
+START_MSG = """
+Click the below to get Your movie  <b><i>RoyalBotz</i></b>""" 
+
+START_BUTTON = InlineKeyboardMarkup(
+    [[
+       InlineKeyboardButton(f"Join Channel", url=f"t.me/RoyalBotz")]
+      ]
+    )

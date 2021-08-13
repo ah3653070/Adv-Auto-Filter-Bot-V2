810037e9bd49db8a9972accdf80a6f06f01858b5
Mammookkafan <87150361+Mammookkafan@users.noreply.github.com>
2021-08-13 18:12:03 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -18,6 +18,8 @@ class Translation(object):
     
 <b>➥ Creator</b> : <b><i><a href="https://t.me/cw_perfect">mr.perfect</a></i></b>
 
+<b>➥ Server</b>  : <b>Paid server</b>
+
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
